e85ca65934b4228561dbafcab53ec6a73f2067f7
KartalToth240560 <240560@buas.nl>
2025-12-17 15:36:00 +0100

    Sad

--- training_rl_ppo.py
+++ training_rl_ppo.py
@@ -54,7 +54,7 @@ from stable_baselines3.common.vec_env import SubprocVecEnv
 from wandb.integration.sb3 import WandbCallback
 
 # Import your custom environment class
-from ot2_twin.ot2_gym_wrapper import OT2Env
+from ot2_gym_wrapper import OT2Env
 
 from clearml import Task
 
